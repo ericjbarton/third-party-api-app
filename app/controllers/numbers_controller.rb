828c1967a66f590ad0eb507084bd3653da9dcd26
ericjbarton <erockbart@gmail.com>
2022-07-21 08:55:49 -0400

    fixes controller, 100 digits of pi now print in browser

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -1,6 +1,6 @@
 class NumbersController < ApplicationController
   def index
-    response = HTTP.get("https://api.math.tools//numbers/pi")
+    response = HTTP.get("https://api.math.tools/numbers/pi")
     render json: response.parse(:json)
   end
 end
